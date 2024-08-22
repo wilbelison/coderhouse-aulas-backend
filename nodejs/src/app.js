8e5fbcc87b2eff0c898e0d8e359e8c26b007b089
Wilbelison Junior <meustudio@gmail.com>
2024-08-22 20:25:21 -0300

    primeiras rotas no express

--- nodejs/src/app.js
+++ nodejs/src/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (request, response) {
-  response.send("Hello World!");
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.get("/html", function (req, res) {
+  res.send("<h1>Html</h1>");
+});
+
+app.get("/json", function (req, res) {
+  res.send({ id: 0, name: "wil"});
 });
 app.listen(8080, () => {
   console.log("Server running on port 8080", "http://localhost:8080");
