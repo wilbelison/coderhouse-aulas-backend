583fd04804b96821040dd18f16d5e3a360c5eaf3
Wilbelison Junior <meustudio@gmail.com>
2024-08-26 07:53:47 -0300

    fix aula 6

--- nodejs/aula_6_express.js
+++ nodejs/aula_6_express.js
@@ -2,19 +2,20 @@ const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
+// http://localhost:8080/?name=wil
+
 app.get("/", function (req, res) {
   const { name } = req.query;
-  res.send(`Hello, ${name}!`);
+  res.send(`<h1>Hello, ${name}!</h1>`);
 });
+
+// http://localhost:8080/wil
 
 app.get("/:name", function (req, res) {
   const { name } = req.params;
   res.send(`<h1>Hello, ${name}!</h1>`);
 });
 
-app.get("/json", function (req, res) {
-  res.send({ id: 0, name: "wil" });
-});
 app.listen(8080, () => {
   console.log("Server running on port 8080", "http://localhost:8080");
 });
